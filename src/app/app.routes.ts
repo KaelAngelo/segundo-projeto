cebee0289d949394ae7195387d00d75004aebeaf
Ezequiel Angelo <ezequiel_angelo_bbu@hotmail.com>
2024-11-25 15:31:41 -0400

    sync

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './components/login/login.component';
+import { PrincipalComponent } from './components/principal/principal.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:"", component: LoginComponent},
+    {path:"principal", component: PrincipalComponent}
+];
