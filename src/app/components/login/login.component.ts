fa74e57cd7fbbfcb8f3f3a07feeaa33e05948b50
Ezequiel Angelo <ezequiel_angelo_bbu@hotmail.com>
2024-12-03 13:31:32 -0400

    teste

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
+import { MdbFormsModule } from 'mdb-angular-ui-kit/forms';
 
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [MdbFormsModule ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
